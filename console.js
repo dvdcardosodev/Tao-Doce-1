b513db42af431276b190f50e329cc45446e7c1ad
dvdcardoso87 <118224711+dvdcardoso87@users.noreply.github.com>
2022-11-15 14:12:36 -0300

    Add files via upload

---
+++ console.js
@@ -0,0 +1,15 @@
+function trocaImagem() {
+    document.getElementById("thumbs").src = "./Images/Brigadeiro1.png";
+}
+
+function trocaImagem1() {
+    document.getElementById("thumbs").src = "./Images/Tematicos1.png";
+}
+
+function trocaImagem2() {
+    document.getElementById("thumbs").src = "./Images/Kits1.png";
+}
+
+function trocaImagem3() {
+    document.getElementById("thumbs").src = "./Images/Bolo1.png";
+}
