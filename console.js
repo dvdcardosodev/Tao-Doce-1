890c161cedf3748ad0458b8bf148080ecfbb05af
dvdcardosodev <dfvcardoso@outlook.com>
2022-12-19 21:26:52 -0300

    Final Changes

--- console.js
+++ console.js
@@ -1,15 +1,15 @@
-function trocaImagem() {
+const trocaImagem= () =>{
     document.getElementById("thumbs").src = "./Images/Brigadeiro1.png";
 }
 
-function trocaImagem1() {
+const trocaImagem1 = ()=> {
     document.getElementById("thumbs").src = "./Images/Tematicos1.png";
 }
 
-function trocaImagem2() {
+const trocaImagem2 = () =>{
     document.getElementById("thumbs").src = "./Images/Kits1.png";
 }
 
-function trocaImagem3() {
+const trocaImagem3 = () => {
     document.getElementById("thumbs").src = "./Images/Bolo1.png";
 }
